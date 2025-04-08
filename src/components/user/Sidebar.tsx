f00ba124d7f9f560dbc8434e9bb4eea3188af022
preetham-fernandes <preethamfernandes31@gmail.com>
2025-04-08 21:12:33 +0530

    sidebar fix

--- src/components/user/Sidebar.tsx
+++ src/components/user/Sidebar.tsx
@@ -6,8 +6,7 @@ import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { 
   Home, 
-  BookOpen, 
-  ClipboardList, 
+  BookOpen,  
   FileText, 
   Users, 
   User, 
